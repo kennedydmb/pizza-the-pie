2b061fcd5dab9f14d19bc30508d7343a22f9a0e2
Ubuntu <ubuntu@ip-172-31-27-57.ec2.internal>
2019-09-11 17:26:19 +0000

    Added test to courses. Added Courses to installed apps

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Course
 
 # Create your tests here.
+class CourseTests(TestCase):
+    """
+    Here we'll define the tests that we'll run against our
+    Product model
+    """
+
+    def test_str(self):
+        test_name = Course(name='A course')
+        self.assertEqual(str(test_name), 'A course')
