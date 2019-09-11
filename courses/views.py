881ee431fe32d4c8e3b7829a62fdfb99c9a19ed6
Ubuntu <ubuntu@ip-172-31-27-57.ec2.internal>
2019-09-11 17:41:53 +0000

    Added view for courses.html

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+def all_courses(request):
+    """
+    This will return all the courses in the database
+    """
+    courses=Course.objects.all()
+    return render(request, courses.html, {"courses":courses})
+    
+    
