b039b6b08ba194a197a0008773d0582cc8a45ad9
Ubuntu <ubuntu@ip-172-31-27-57.ec2.internal>
2019-09-12 10:57:59 +0000

    Cart in installed apps. Added cart to contect processors

--- piy/settings.py
+++ piy/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'accounts',
-    'courses'
+    'courses',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media'
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
