b26902dd032f52e1dd7fc25942b24f0fcfc6f5a7
Ubuntu <ubuntu@ip-172-31-27-57.ec2.internal>
2019-09-12 10:58:26 +0000

    Added contexts, urls and views

---
+++ cart/contexts.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404
+from courses.models import Course
+
+
+def cart_contents(request):
+    """
+    Ensures that the cart contents are available when rendering
+    every page
+    """
+    cart = request.session.get('cart', {})
+
+    cart_items = []
+    total = 0
+    course_count = 0
+    
+    for id, quantity in cart.items():
+        course = get_object_or_404(Course, pk=id)
+        total += quantity * course.price
+        course_count += quantity
+        cart_items.append({'id': id, 'quantity': quantity, 'course': course})
+    
+    return {'cart_items': cart_items, 'total': total, 'course_count': course_count}
