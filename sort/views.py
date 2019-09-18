a00e70d55e76d8755da93afd3526ce39634db564
Ubuntu <ubuntu@ip-172-31-27-57.ec2.internal>
2019-09-18 19:03:18 +0000

    Tidied up styling for courses, cart and checkout

--- sort/views.py
+++ sort/views.py
@@ -1,20 +1,65 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from courses.models import Course
 
 # Create your views here.
 def price_ascend(request):
-    courses=Course.objects.order_by('price')
+    courses_list=Course.objects.order_by('price')
+    paginator = Paginator(courses_list, 6) # Show 6 courses per page
+
+    page = request.GET.get('page')
+    try:
+        courses = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        courses = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        courses = paginator.page(paginator.num_pages)
     return render(request, "courses.html", {"courses": courses})
     
 def price_descend(request):
-    courses=Course.objects.order_by('-price')
+    courses_list=Course.objects.order_by('-price')
+    paginator = Paginator(courses_list, 6) # Show 6 courses per page
+
+    page = request.GET.get('page')
+    try:
+        courses = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        courses = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        courses = paginator.page(paginator.num_pages)
     return render(request, "courses.html", {"courses": courses})
     
 def name_ascend(request):
-    courses=Course.objects.order_by('name')
+    courses_list=Course.objects.order_by('name')
+    paginator = Paginator(courses_list, 6) # Show 6 courses per page
+
+    page = request.GET.get('page')
+    try:
+        courses = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        courses = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        courses = paginator.page(paginator.num_pages)
     return render(request, "courses.html", {"courses": courses})
     
 def name_descend(request):
-    courses=Course.objects.order_by('-name')
+    courses_list=Course.objects.order_by('-name')
+    paginator = Paginator(courses_list, 6) # Show 6 courses per page
+
+    page = request.GET.get('page')
+    try:
+        courses = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        courses = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        courses = paginator.page(paginator.num_pages)
     return render(request, "courses.html", {"courses": courses})
     
